e0c45e64b9edfb6b50ddb29ead8a0c5c5c5dddfb
Dimitri Marchetti <dimitri.marchetti@gmail.com>
2020-04-08 22:24:55 -0300

    Validacoes do backend e instalacao do Jest Ok

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const { celebrate, Segments, Joi } = require('celebrate')
 
 // importa OngController
 const OngController = require('./controllers/OngController')
@@ -10,22 +11,68 @@ const SessionController = require('./controllers/SessionController')
 // importa rotas
 const routes = express.Router()
 
+
+routes.post('/sessions', celebrate({
+  [Segments.BODY]: Joi.object().keys({
+    id: Joi.string().required()
+  })
+}), SessionController.create)
+
 // Rota get - retorna todas as ongs
 routes.get('/ongs', OngController.index)
 
-// Rota post - async - transforma a função em assíncrona
-routes.post('/ongs', OngController.create)
+/*
+ * Query
+ * Route
+ * Body
+ */
 
-// IncidentsController routes 
-routes.get('/incidents', IncidentController.index )
-routes.post('/incidents', IncidentController.create )
-routes.delete('/incidents/:id', IncidentController.delete)
 
-// ProfileController routes
-routes.get('/profile', ProfileController.index)
+// Rota post - cria Ongs 
+// Usando Joi e Celebrate para fazer as validações dos campos de recebimento.
+routes.post('/ongs', celebrate({
+  [Segments.BODY]: Joi.object().keys({
+    name: Joi.string().required(),
+    email: Joi.string().required().email(),
+    whatsapp: Joi.string().required().min(10).max(11),
+    city: Joi.string().required(),
+    uf: Joi.string().required().length(2)
+  })
+}), OngController.create)
 
-//SessionController routes
-routes.post('/sessions', SessionController.create)
+// Rota Mostra casos
+routes.get('/incidents', celebrate({
+  [Segments.QUERY]: Joi.object().keys({
+    page: Joi.number(),
+  })
+}), IncidentController.index )
 
+// Rota Cria Casos 
+routes.post('/incidents', celebrate({
+  [Segments.BODY]: Joi.object().keys({
+    title: Joi.string().required(),
+    description: Joi.string().required(),
+    value: Joi.number().required()
+  }),
+
+  [Segments.HEADERS]: Joi.object({
+    authorization: Joi.string().required(),
+  }).unknown(),
+
+}), IncidentController.create )
+
+// Rota Apaga Casos
+routes.delete('/incidents/:id', celebrate({
+  [Segments.PARAMS]: Joi.object().keys({
+    id: Joi.number().required()
+  })
+}), IncidentController.delete)
+
+// Rota mostra ongs e casos
+routes.get('/profile', celebrate({
+  [Segments.HEADERS]: Joi.object({
+    authorization: Joi.string().required(),
+  }).unknown(),
+}), ProfileController.index)
 
 module.exports = routes
